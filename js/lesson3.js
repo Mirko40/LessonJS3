9629eca8f5130742a783eae3501bc7efa8012993
Mirko40 <mmirko@ua.fm>
2021-11-18 23:19:11 +0200

    initial

--- js/lesson3.js
+++ js/lesson3.js
@@ -12,3 +12,20 @@ function getSumRange() {
     alert(`The sum of numbers ${sum}`)
 };
 
+function getLargestDivisor() {
+    let a = Number(prompt("Enter the first number", ""));
+    let b = Number(prompt("Enter the second number", ""));
+    a = Math.abs(a);
+    b = Math.abs(b);
+
+    if (a == 0) {
+        alert(`The largest common divisor ${b}`)
+    }
+    else while (b != 0) {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    };
+    alert(`The largest common divisor ${a}`);
+};
